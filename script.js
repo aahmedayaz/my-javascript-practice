d8ade5fa3728bd1578e1d2e2d473300e79bed916
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-08-31 04:10:28 +0500

    Unshift Using For Loop

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
+// //  Push Function
+
+// let x = []
+// for (let index = 0; index < 10; index++) {
+//         x.push(`Push - ${index + 1} == ` + index);      
+// }
+// for (let i = 0; i < x.length; i++) {
+//     document.write(x[i] + " ");
+//     document.write(typeof x[i] + "<br>");
+//     if (i%2 != 0) {
+//         document.write("<br>");
+//     }
+// }
+
+// // Output :
+
+// // Push - 1 == 0 string
+// // Push - 2 == 1 string
+
+// // Push - 3 == 2 string
+// // Push - 4 == 3 string
+
+// // Push - 5 == 4 string
+// // Push - 6 == 5 string
+
+// // Push - 7 == 6 string
+// // Push - 8 == 7 string
+
+// // Push - 9 == 8 string
+// // Push - 10 == 9 string
 
 
-let x = []
-for (var index = 0; index < 10; index++) {
-        x.push(`Push - ${index + 1} == ` + index);      
-}
-for (var i = 0; i < x.length; i++) {
-    document.write(x[i] + " ")
-    document.write(typeof x[i] + "<br>")
-    if (i%2 != 0) {
-        document.write("<br>")
-    }
-}
+// // Unshift Fucntion 
+
+// let y = []
+// for (let index = 0; index < 10; index++) {
+//     if(index == 4){
+//         y.unshift("ahmed");
+//     }
+//     else{
+//         y.unshift(index);
+//     }
+              
+// }
+
+// for (let index = 0; index < y.length; index++) {
+//         let a = y[index];
+//         document.write(y[index] + " " + typeof a + "<br>")
+// }
+
+// // Output :
+
+// // 9 number
+// // 8 number
+// // 7 number
+// // 6 number
+// // 5 number
+// // ahmed string
+// // 3 number
+// // 2 number
+// // 1 number
+// // 0 number
+
+
+
+
+
+
+
+
+
+
