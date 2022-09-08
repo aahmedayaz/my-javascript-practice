6b3ae0498a3f6eaa4f76fc2ac03a257592992cc3
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-09-09 03:00:33 +0500

    StringMethods Completed

---
+++ step-05-StringMethods/script.js
@@ -0,0 +1,300 @@
+// Strings in JavaScript  ****/
+
+// A JavaScript string is zero or more characters written inside quotes.
+
+// JavaScript strings are used for storing and manipulating text.
+// You can use single or double quotes
+
+// Strings can be created as primitives, 
+// from string literals, or as objects, using the String() constructor
+
+let myName = "Ahmed Ayaz";
+let myChannelName = 'Ahmed Ayaz';
+
+let ytName = 'University is a JOke';
+
+console.log(myName);
+console.log((ytName));
+
+
+
+// --->  How to find the length of a string
+// String.prototype.length 
+// Reflects the length of the string. 
+
+console.log(myName.length);
+
+
+
+// ---> Escape Character
+
+let anySentence = "We are the so-called \"Vikings\" from the north.";
+console.log(anySentence);
+
+// if you dont want to mess, simply use the alternate quotes 
+
+let anySentence1 =  " We are the so-called 'Vikings' from the north. ";
+console.log(anySentence1);
+
+
+
+
+
+
+
+
+
+// ----> Finding a String in a String
+
+// String.prototype.indexOf(searchValue [, fromIndex]) 
+
+// The indexOf() method returns the index of (the position of) the first
+//  occurrence of a specified text in a string
+
+const myBioData =  'I am the University is a JOke';
+console.log(myBioData.indexOf("t", 6));
+
+
+// JavaScript counts positions from zero.
+// 0 is the first position in a string, 1 is the second, 2 is the third ...
+
+
+
+
+
+
+// String.prototype.lastIndexOf(searchValue [, fromIndex]) 
+// Returns the index within the calling String object of the 
+// last occurrence of searchValue, or -1 if not found.
+
+console.log(myBioData.lastIndexOf("t", 6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----> Searching for a String in a String
+
+// String.prototype.search(regexp) 🙋‍♂️
+
+// The search() method searches a string for a specified 
+// value and returns the position of the match
+
+let sData = myBioData.search("JOke");
+console.log(sData);
+
+// The search() method cannot take a second start position argument.
+
+// ----> Extracting String Parts
+
+// There are 3 methods for extracting a part of a string:
+
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+
+// The slice() Method 
+// slice() extracts a part of a string and returns the extracted part 
+// in a new string.
+
+// The method takes 2 parameters: the start position, 
+// and the end position (end not included).
+
+var str = "Apple, Bananaa, Kiwi, mango";
+
+// let res = str.slice(0,4);
+let res = str.slice(7);
+console.log(res);
+
+
+// The slice() method selects the elements starting at the 
+// given start argument, and ends at, but does not include, 
+// the given end argument.
+
+// Note: The original array will not be changed.
+// Remember: JavaScript counts positions from zero. First position is 0.
+
+
+// Q # Display only 280 characters of a string like the 
+// one used in Twitter?
+
+let myTweets  = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? ";
+
+let myActualTweet = myTweets.slice(0,280);
+console.log(myActualTweet);
+console.log(myActualTweet.length);
+
+
+
+
+
+
+// The substring() Method 
+// substring() is similar to slice().
+
+// The difference is that substring() cannot accept 
+// negative indexes.
+
+var str = "Apple, Bananaa, Kiwi";
+let res2 = str.substring(8,-2);
+console.log(res2);
+
+
+
+// If we give negative value then the characters are
+//  counted from the 0th pos 
+
+
+
+// The substr() Method 🙋‍♂️
+// substr() is similar to slice().
+
+// The difference is that the second parameter specifies the 
+// length of the extracted part.
+
+
+var str = "Apple, Bananaa, Kiwi";
+// let res = str.substr(7,-2);
+let res1 = str.substr(-4);
+console.log(res1);
+
+
+
+
+
+
+// ---->  Replacing String Content() 
+
+// String.prototype.replace(searchFor, replaceWith) 
+
+// The replace() method replaces a specified value 
+// with another value in a string.
+
+let myBioData1 = `I am Ahmed Ayaz`;
+
+let repalceData = myBioData1.replace('Ahmed','AHMED');
+console.log(repalceData);
+console.log(myBioData1);
+
+
+// Points to remember 
+// 1: The replace() method does not change the string 
+// it is called on.  It returns a new string.
+// 2: By default, the replace() method replaces only 
+// the first match
+// 3:By default, the replace() method is case sensitive. 
+
+
+
+
+
+
+
+
+// ---->  Extracting String Characters
+
+// There are 3 methods for extracting string characters:
+
+// charAt(position)
+// charCodeAt(position)
+// Property access [ ]
+
+
+
+// The charAt() Method 
+// The charAt() method returns the character at a 
+// specified index (position) in a string
+
+let str = "HELLO WORLD";
+
+console.log(str.charAt(9));
+
+
+
+
+
+
+
+
+
+
+// The charCodeAt() Method 
+// The charCodeAt() method returns the unicode of the 
+// character at a specified index in a string:
+
+// The method returns a UTF-16 code
+//  (an integer between 0 and 65535).
+
+// The Unicode Standard provides a unique number for every 
+// character, no matter the platform, device, application, 
+// or language. UTF-8 is a popular Unicode encoding which
+//  has 88-bit code units.
+
+
+
+var str = "HELLO WORLD";
+
+console.log( str.charCodeAt(0) );
+
+
+
+// ----> challenge Time 
+
+// Return the Unicode of the last character in a string
+
+let str = "HELLO WORLD";
+let lastChar = str.length - 1;
+console.log(str.charCodeAt(lastChar));
+
+
+let myName1 = "Ahmed Ayaz";
+console.log(myName1.toUpperCase()); 
+console.log(myName1.toLowerCase());
+
+
+// The concat() Method 
+// concat() joins two or more strings
+
+let fName = "Ahmed"
+let lName = "Ayaz"
+
+console.log(fName + lName );
+console.log(`${fName} ${lName}`);
+console.log(fName.concat(lName));
+console.log(fName.concat(" " ,lName));
+
+
+
+
+
+
+// ----> String.trim() 
+// The trim() method removes whitespace from both 
+// sides of a string
+
+var str = "              Hello         World!            ";
+console.log(str.trim());
+
+
+// Converting a String to an Array
+// A string can be converted to an array with the 
+// split() method
+
+var txt = "a, b,c d,e";   // String
+console.log(txt.split(","));           // Split on commas
+console.log( txt.split(" "));          // Split on spaces
+console.log(txt.split("|"));         // Split on pipe
+
+
+
+
+
+
